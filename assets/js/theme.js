1819cc512759aa58ee566c5d3c255d9fcd295d85
chamberofsecretz <64255683+chamberofsecretz@users.noreply.github.com>
2020-09-13 17:00:49 +0300

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -26,47 +26,47 @@ jQuery(document).ready(function ($) {
 (function ($) {
     "use strict";
 
-//     var form = document.getElementById("form");
-//     var button = document.getElementById("contact-form-submit");
-//     var status = document.getElementById("contact-form-status");
+    var form = document.getElementById("form");
+    var button = document.getElementById("contact-form-submit");
+    var status = document.getElementById("contact-form-status");
 
 
-//     function success() {
-//         form.reset();
-//         button.style = "display: none";
-//         status.style = "color: #ffffff; text-align: center; padding-top: 20px";
-//         status.innerHTML = "הטופס נשלח בהצלחה! </br> נחזור אליכם בהקדם";
-//     }
+    function success() {
+        form.reset();
+        button.style = "display: none";
+        status.style = "color: #ffffff; text-align: center; padding-top: 20px";
+        status.innerHTML = "הטופס נשלח בהצלחה! </br> נחזור אליכם בהקדם";
+    }
 
-//     function error() {
-//         status.innerHTML = "אוי לא, קרתה שגיאה";
-//         status.style = "color: red ";
-//     }
+    function error() {
+        status.innerHTML = "אוי לא, קרתה שגיאה";
+        status.style = "color: red ";
+    }
 
-    // handle the form submission event
+//     handle the form submission event
 
-//     form.addEventListener("submit", function (ev) {
-//         ev.preventDefault();
-//         var data = new FormData(form);
-//         ajax(form.method, form.action, data, success, error);
-//     });
+    form.addEventListener("submit", function (ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
 
-//     // helper function for sending an AJAX request
+    // helper function for sending an AJAX request
 
-//     function ajax(method, url, data, success, error) {
-//         var xhr = new XMLHttpRequest();
-//         xhr.open(method, url);
-//         xhr.setRequestHeader("Accept", "application/json");
-//         xhr.onreadystatechange = function () {
-//             if (xhr.readyState !== XMLHttpRequest.DONE) return;
-//             if (xhr.status === 200) {
-//                 success(xhr.response, xhr.responseType);
-//             } else {
-//                 error(xhr.status, xhr.response, xhr.responseType);
-//             }
-//         };
-//         xhr.send(data);
-//     }
+    function ajax(method, url, data, success, error) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState !== XMLHttpRequest.DONE) return;
+            if (xhr.status === 200) {
+                success(xhr.response, xhr.responseType);
+            } else {
+                error(xhr.status, xhr.response, xhr.responseType);
+            }
+        };
+        xhr.send(data);
+    }
 
     //Submenu Dropdown Toggle
 
