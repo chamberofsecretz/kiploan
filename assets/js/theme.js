6e73977e1a2b4ce7d20f7c9f72783e01a3a731b1
chamberofsecretz <64255683+chamberofsecretz@users.noreply.github.com>
2020-10-20 00:52:04 +0300

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -64,30 +64,34 @@ jQuery(document).ready(function ($) {
         status.innerHTML = "הטופס נשלח בהצלחה! </br> נחזור אליכם בהקדם";
     }
 
-    // function error() {
-    //     status.innerHTML = "אוי לא, קרתה שגיאה";
-    //     status.style = "color: red ";
-    // }
+    function error() {
+        status.innerHTML = "אופס, קרתה שגיאה";
+        status.style = "color: red ";
+    }
 
     //     handle the form submission event
 
-    form.addEventListener("submit", function (ev) {
-        ev.preventDefault();
-        var data = new FormData(form);
-        ajax(form.method, form.action, data, success);
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
     });
 
     // helper function for sending an AJAX request
 
-    function ajax(method, url, data, success) {
-        var xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.setRequestHeader("Accept", "application/json");
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState !== XMLHttpRequest.DONE) return;
-            success(xhr.response, xhr.responseType);
-        };
-        xhr.send(data);
+    function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
     }
 
     //Submenu Dropdown Toggle
